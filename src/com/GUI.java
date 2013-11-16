ea88ebb41f3e88d98239b650a6f3b2208fd153ad
Sarabeth Jaffe <jaffes2@rpi.edu>
2013-11-16 14:18:01 -0500

    Testing Simulation

--- src/com/GUI.java
+++ src/com/GUI.java
@@ -17,6 +17,9 @@ import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.geom.AffineTransform;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Vector;
 
 import javax.swing.AbstractAction;
 import javax.swing.JFrame;
@@ -24,12 +27,13 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
-public class GUI extends JPanel {
+public class GUI extends JPanel implements ActionListener {
 
-	//global variables
+	// global variables
 	JFrame Frame1 = new JFrame("TestFrame");
 	RoadMap r = new RoadMap();
-	Road road = r.road;
+	Road road = r.road;//roadmap only contains one road for now -> for simplicity sake
+	BouncingBall b;
 	
 	public GUI() {
 
@@ -39,74 +43,85 @@ public class GUI extends JPanel {
 
 		JPanel panel = new JPanel(new GridLayout(0, 2));
 
+		Frame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		// set the background to be gray
-		panel.setBackground(Color.gray);
-
-		// add "ovals" or cars to panel
-		panel.add(new OvalComponent());
-
-		// add "rectangles" or roads to panel
-		panel.add(new RectangleComponent());
+		//panel.setBackground(Color.gray);
+		
+		b = new BouncingBall(20, 20, 4, 0);
+		Frame1.add(b);
+		//Frame1.add(new BouncingBall(10, 20, 4, 0));
 
 		// add Panel to frame
 		Frame1.add(panel);
+		
+		// a timer called every 1/10 second
+		Timer timer = new Timer(100, this);
+		timer.start();
 	}
 
-	class OvalComponent extends JComponent {
+	// called by the Timer
+	public void paintComponent(Graphics g) {
 
-		@Override
-		public void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			g.setColor(Color.yellow);
-			g.fillOval(20, 20, 20, 20);
-			
-			try {
-				Thread.sleep(2);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			g.fillOval(30, 20, 20, 20);
-
-			//g.translate(25, 25);
-		}
+		super.paintComponent(g);
+				
+		g.setColor(Color.gray);
 		
+		g.fillRect(0, 20, 1000, 50);
+
+		g.fillRect(0, 20, 50, 1000);
 		
 	}
-
-	class RectangleComponent extends JComponent {
-
-		@Override
-		public void paintComponent(Graphics g) {
-
-			super.paintComponent(g);
-			Graphics2D g2d = (Graphics2D) g;
-			g2d.setColor(Color.black);
-			Rectangle rect2 = new Rectangle((int)road.X, (int)road.Y, 20, road.getLength());
-
-			//g2d.draw(rect2);
-			//g2d.rotate(Math.toRadians(30));
-			g2d.draw(rect2);
-			g2d.fill(rect2);
-
-			
-			/*
-			AffineTransform transform = new AffineTransform();
-			transform.rotate(Math.toRadians(45), rect2.getX() + rect2.width/2, rect2.getY() + rect2.height/2);
-			g2d.fill(rect2);
-			
-			AffineTransform old = g2d.getTransform();
-			g2d.transform(transform);
-			
-			
-			g2d.draw(rect2);*/
-		}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		
+		repaint();
 	}
 
 	public static void main(String[] args) {
 
 		new GUI();
+		
+		RoadMap r = new RoadMap();
+		Road road = r.road;
+		Lane[] lane = road.lanes;
+		lane[0].addCar(new Car(2.2, 0));
+		lane[0].addCar(new Car(2.2, 1));
+		
+		System.out.println("Lane has " + lane.length + "cars");
+		
+		lane[0].cars.get(0).velocity = 5.0;//setting the velocity to 5 just for shits and giggles
+		lane[0].cars.get(1).velocity = 8.0;
+		
+		LinkedList<Car> cars_ = lane[0].cars;//gets the list of cars from the Lane
+		
+		ListIterator<Car> itr = cars_.listIterator();
+		
+		Vector<BouncingBall> balls = new Vector();
+		balls.add(new BouncingBall(0, 0, 5.0, 0));
+		balls.add(new BouncingBall(0, 0, 8.0, 0));
+
+		while(itr.hasNext()){
+			
+			Car c = itr.next();
+			
+			System.out.println(c.serialNum + "was added to the GUI");
+			
+			//frame.add(new BouncingBall(0, 0, c.velocity, 0));
+		}
+
+		/*
+		// a JFrame to test the whole thing
+		JFrame frame = new JFrame("Bouncing ball");
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		// add the bouncing panel to the JFrame
+		frame.add(new BouncingBall(20, 20, 4, 0));
+		frame.add(new BouncingBall(0, 20, 4, 0));
+
+		//frame.add(new Road(road.x, road.y, length, orientation) );
+
+		frame.setVisible(true);*/
 
 	}
 }
